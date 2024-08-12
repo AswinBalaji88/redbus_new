ef9f98efd7a255225186207da09f4d1b5e70d634
AswinBalaji88 <167520681+AswinBalaji88@users.noreply.github.com>
2024-08-13 02:19:31 +0530

    Update Online_bus_app.py

--- Online_bus_app.py
+++ Online_bus_app.py
@@ -5,9 +5,9 @@ import pandas as pd
 # Connect to MySQL database
 def get_connection():
     return pymysql.connect(
-    host="localhost",
+    host="127.0.0.1",
     user="root",
-    password="",
+    password="12345678",
     database="redbus")
 
 # Function to fetch route names starting with a specific letter, arranged alphabetically
